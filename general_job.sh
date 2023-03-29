cf502fe14d0ca9acca8a840430ba54c02cb7cce9
gyakkun <xiaowufeixia@gmail.com>
2023-03-29 19:59:14 +0800

    Update job to support blog.

--- general_job.sh
+++ general_job.sh
@@ -4,7 +4,7 @@ TOPIC_TYPE="$1" # group, subject
 
 G_PWD=`pwd`
 G_RET=""
-G_TOPIC_TYPE_ARR=("group" "subject")
+G_TOPIC_TYPE_ARR=("group" "subject" "blog")
 
 source $G_PWD/env.sh
 source $G_PWD/util.sh
@@ -36,11 +36,13 @@ G_BLOG_AVOID_LIST=$E_BGM_BLOG_AVOID_LIST
 
 
 BGM_RAUKEN_TOPICLIST_URL_TEMPLATE="https://%s/rakuen/topiclist?type=$TOPIC_TYPE"
-BGM_GROUP_TOPIC_URL_TEMPLATE="https://%s/m/topic/$TOPIC_TYPE"
+BGM_TOPIC_URL_TEMPLATE="https://%s/m/topic/$TOPIC_TYPE"
+BGM_BLOG_URL_TEMPLATE="https://%s/blog"
 BGM_DOMAIN_LIST=$E_BGM_DOMAIN_LIST
 
 TMP_BGM_RAUKEN_TOPICLIST_URL=
-TMP_BGM_GROUP_TOPIC_URL=
+TMP_BGM_TOPIC_URL=
+TMP_BGM_BLOG_URL=
 
 SUCCESS_COUNTER=0
 FAILURE_COUNTER=0
@@ -65,6 +67,7 @@ print_info Current Time ISO 8601: $G_RET
 
 # Get NG list from file
 declare -a ng_list
+touch $G_GIT_REPO_DIR/$TOPIC_TYPE/ng.txt
 ng_list=(`cat $G_GIT_REPO_DIR/$TOPIC_TYPE/ng.txt | sort -u | uniq`) # > $G_GIT_REPO_DIR/$TOPIC_TYPE/ng.txt
 #readarray -t ng_list < $G_GIT_REPO_DIR/$TOPIC_TYPE/ng.txt
 > $G_GIT_REPO_DIR/$TOPIC_TYPE/ng.txt
@@ -77,6 +80,12 @@ print_info Random Bangumi domain picked : $G_RET
 printf -v TMP_BGM_RAUKEN_TOPICLIST_URL "$BGM_RAUKEN_TOPICLIST_URL_TEMPLATE" $G_RET
 curlToFile $TMP_BGM_RAUKEN_TOPICLIST_URL $G_GIT_REPO_DIR/$TOPIC_TYPE/rakuen_topic_list.html
 topic_list=(`grep -Po '(?<=href="/rakuen/topic/'$TOPIC_TYPE'/)[0-9]+' $G_GIT_REPO_DIR/$TOPIC_TYPE/rakuen_topic_list.html | sort -u | uniq`)
+
+# Blog
+if [ "$TOPIC_TYPE" == "blog" ]; then
+	topic_list=(`grep -Po '(?<=href="/blog/)[0-9]+' $G_GIT_REPO_DIR/$TOPIC_TYPE/rakuen_topic_list.html | sort -u | uniq`)
+fi
+
 # Clear
 > $G_GIT_REPO_DIR/$TOPIC_TYPE/topiclist.txt
 # Write
@@ -84,7 +93,7 @@ for i in ${topic_list[@]}
 do
 	echo "$i" >> $G_GIT_REPO_DIR/$TOPIC_TYPE/topiclist.txt
 done
-print_success TOPIC LIST: ${topic_list[@]}
+print_success ${TOPIC_TYPE^^} LIST: ${topic_list[@]}
 # Shuffle the topic list
 topic_list=(`shuf -e ${topic_list[@]}`)
 
@@ -94,36 +103,43 @@ function archive() {
 	for i in ${arr[@]}
 	do
 		print_info archiving $TOPIC_TYPE topic $i
-		if [[ " ${G_GROUP_TOPIC_AVOID_LIST[@]} " =~ " $i " ]]
-			then
-				print_warning $i is in AVOID LIST
-		else
-			ten_thousand=$(expr $i / 10000)
-			printf -v ten_thousand "%02d" $ten_thousand
-			hundred=$(expr $(expr $i % 10000) / 100)
-			printf -v hundred "%02d" $hundred
-			output_dir=$G_GIT_REPO_DIR/$TOPIC_TYPE/$ten_thousand/$hundred
-			output_loc=$output_dir/$i.html
-			mkdir -p $output_dir
-			randBgmDomain
-			printf -v TMP_BGM_GROUP_TOPIC_URL "$BGM_GROUP_TOPIC_URL_TEMPLATE" $G_RET
-			printCurrentTimeISO
-			print_info $TMP_BGM_GROUP_TOPIC_URL/$i to $output_loc
-			curlToFile $TMP_BGM_GROUP_TOPIC_URL/$i $output_loc
-			if [[ $G_RET -eq 0 ]]
-			then
-				trimHtml $output_loc
-				[[ $DURING_NG -eq 0 ]] && tidyHtml $output_loc
-				((SUCCESS_COUNTER++))
-				print_success SUCCESS_COUNTER: $SUCCESS_COUNTER
-			else
-				print_error Archiving $TOPIC_TYPE id $i failed. Writing to ng.txt
-				echo "$i" >> $G_GIT_REPO_DIR/$TOPIC_TYPE/ng.txt
-				((FAILURE_COUNTER++))
-				print_error FAILURE_COUNTER: $FAILURE_COUNTER
-			fi
-			sleep $E_SLEEP_PERIOD
+		if [[  "$TOPIC_TYPE"=="group" &&  " ${G_GROUP_TOPIC_AVOID_LIST[@]} " =~ " $i " ]];then
+			print_warning $i is in AVOID LIST of $TOPIC_TYPE
+			continue
 		fi
+		if [[ "$TOPIC_TYPE"=="subject" &&" ${G_SUBJECT_TOPIC_AVOID_LIST[@]} " =~ " $i " ]];then
+			print_warning $i is in AVOID LIST of $TOPIC_TYPE
+			continue
+		fi
+		if [[ "$TOPIC_TYPE"=="blog" &&" ${G_BLOG_AVOID_LIST[@]} " =~ " $i " ]];then
+			print_warning $i is in AVOID LIST of $TOPIC_TYPE
+			continue
+		fi
+		ten_thousand=$(expr $i / 10000)
+		printf -v ten_thousand "%02d" $ten_thousand
+		hundred=$(expr $(expr $i % 10000) / 100)
+		printf -v hundred "%02d" $hundred
+		output_dir=$G_GIT_REPO_DIR/$TOPIC_TYPE/$ten_thousand/$hundred
+		output_loc=$output_dir/$i.html
+		mkdir -p $output_dir
+		randBgmDomain
+		printf -v TMP_BGM_TOPIC_URL "$BGM_TOPIC_URL_TEMPLATE" $G_RET
+		printCurrentTimeISO
+		print_info $TMP_BGM_TOPIC_URL/$i to $output_loc
+		curlToFile $TMP_BGM_TOPIC_URL/$i $output_loc
+		if [[ $G_RET -eq 0 ]]
+		then
+			trimHtml $output_loc
+			[[ $DURING_NG -eq 0 ]] && tidyHtml $output_loc
+			((SUCCESS_COUNTER++))
+			print_success SUCCESS_COUNTER: $SUCCESS_COUNTER
+		else
+			print_error Archiving $TOPIC_TYPE id $i failed. Writing to ng.txt
+			echo "$i" >> $G_GIT_REPO_DIR/$TOPIC_TYPE/ng.txt
+			((FAILURE_COUNTER++))
+			print_error FAILURE_COUNTER: $FAILURE_COUNTER
+		fi
+		sleep $E_SLEEP_PERIOD
 		currentTimeMills
 		tmp_timing=$G_RET
 		print_info Timing: $(($tmp_timing - $START_TIME))ms
