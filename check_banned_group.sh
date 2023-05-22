98f41de1ede33421f0d78d86facfb921011f03b9
gyakkun <xiaowufeixia@gmail.com>
2023-05-22 12:27:54 +0000

    Add check banned group script.

---
+++ check_banned_group.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#PWD=`pwd`
+
+source ~/source/bgm-archive-sh/env.sh
+
+banned_group=("adc_zero" "guiwubangumi" "kink" "liujun" "witness")
+banned_file=~/source/bgm-archive/group/bn.txt
+time_hour=`date -Is | awk -F'T' '{printf $2}' | awk -F: '{printf $1}'`
+
+echo time_hour $time_hour
+
+[ $time_hour -ne '20'  ] && exit 1
+
+for i in  ${banned_group[@]};do
+	echo checking $i
+	tmpfile=`mktemp`
+	#echo $E_BGM_COOKIE_FILE
+	#echo $E_BGM_UA
+	curl -sL -b $E_BGM_COOKIE_FILE -A "$E_BGM_UA" --output $tmpfile "https://bgm.tv/group/$i"
+	topic_list=(`grep -v '>Mobile' $tmpfile |  grep -Po '(?<=href="/group/topic/)[0-9]+(?!">Mobile)' | sort -rn | uniq`)
+	echo ${topic_list[@]}
+	printf "%s\n" "${topic_list[@]}" >> $banned_file
+	rm -rf $tmpfile
+	sleep 1
+done
+
+# filter empty line
+#cat $banned_file
+cat $banned_file | awk NF | sort -rn > $banned_file
+#cat $banned_file
